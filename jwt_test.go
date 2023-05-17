4ecb82a061d1f72bb4bf8dec912567542ef2dc4d
Jacob Dearing <jacob@dearing.io>
2023-05-17 12:09:49 -0500

    remove unused test

--- jwt_test.go
+++ jwt_test.go
@@ -48,10 +48,6 @@ func TestJWT(t *testing.T) {
 		t.Errorf("JWT token expiry should be later than issued date  %v >= %v", jwt.Payload.ExpiresAt, jwt.Payload.IssuedAt)
 	}
 
-	// if time.Unix(Timestamp(decodedJwt.Payload.IssuedAt).tim, 0).Sub(Timestamp(decodedJwt.Payload.ExpiresAt).Time()) != time.Hour {
-	// 	t.Errorf("JWT token expiry should be 1 hour after issued date  %v != %v", time.Unix(decodedJwt.Payload.ExpiresAt, 0).Sub(time.Unix(decodedJwt.Payload.IssuedAt, 0)), time.Hour)
-	// }
-
 	// check that the expiry is one hour after the issued date
 	if time.Unix(int64(decodedJwt.Payload.ExpiresAt), 0).Sub(time.Unix(int64(decodedJwt.Payload.IssuedAt), 0)) != time.Hour {
 		t.Errorf("JWT token expiry should be 1 hour after issued date  %v != %v", time.Unix(int64(decodedJwt.Payload.ExpiresAt), 0).Sub(time.Unix(int64(decodedJwt.Payload.IssuedAt), 0)), time.Hour)
